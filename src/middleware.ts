f1981d4db1beca9bd922efb98799ff3b8014166b
shivam anand <shivam850anand@gmail.com>
2024-12-18 20:34:55 +0530

    corected

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,12 +8,9 @@ export default clerkMiddleware(async (auth, request) => {
   }
 });
 
-// Matcher configuration
 export const config = {
   matcher: [
-    // Match all routes except those starting with _next or static file extensions
     "/((?!_next|.*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Include all API routes
     "/api/:path*",
   ],
 };
